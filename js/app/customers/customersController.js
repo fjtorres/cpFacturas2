595dcf82e6fa5cb4dd3d1dab700c2c266290706e
Francisco Javier Torres <kisco1984@gmail.com>
2015-02-03 16:44:07 +0100

    - Add pagination to customer list. - Add confirmation modal to customer delete operation.

--- js/app/customers/customersController.js
+++ js/app/customers/customersController.js
@@ -1,14 +1,17 @@
 'use strict';
 
-angular.module('cpFacturasApp').controller('customersController', ['$scope', '$rootScope', '$routeParams', 'customerService', CustomersController]);
+angular.module('cpFacturasApp').controller('customersController', ['$scope', '$rootScope', '$routeParams', '$modal', '$log', 'customerService', CustomersController]);
 
-function CustomersController ($scope, $rootScope, $routeParams, customerService) {
+function CustomersController ($scope, $rootScope, $routeParams, $modal, $log, customerService) {
     expandTable("#btnExpanCustomerList", "#customer-list");
 
     // Clear scope vars
     $scope.model = {};
     $scope.items = [];
     $scope.isUpdate = false;
+    $scope.currentPage = 1;
+    $scope.totalItems = 0;
+    $scope.itemsPerPage = 10;
 
     if ($routeParams.itemId != undefined) {
         $scope.model = customerService.findById($routeParams.itemId);
@@ -27,13 +30,39 @@ function CustomersController ($scope, $rootScope, $routeParams, customerService)
         
     };
 
+    $scope.confirmDelete = function(item){
+        
+        var modalInstance = $modal.open({
+          templateUrl: 'js/app/main/confirmation.tpl.html',
+          controller: 'confirmController',
+          size: 'md',
+          resolve: {
+            title: function(){return "messages.confirmation.title";},
+            body: function () {return "customers.messages.delete.confirmation";}
+          }
+        });
+
+        modalInstance.result.then(function () {
+          $scope.delete(item);
+        }, function () {
+          // Empty
+        });
+    };
+
     $scope.delete = function (item) {
         customerService.delete(item);
         $rootScope.$broadcast('successMessage', "Delete customer");
     };
 
     $scope.search = function () {
-        $scope.items = customerService.search();
+        var page = customerService.search($scope.currentPage - 1, $scope.itemsPerPage);
+        $scope.items = page.data;
+        $scope.totalItems = page.total;
+    };
+
+    $scope.pageChanged = function () {
+        $log.log('Page changed to: ' + $scope.currentPage);
+        $scope.search();
     };
 
     $scope.search();
--- js/app/customers/customersService.js
+++ js/app/customers/customersService.js
@@ -5,13 +5,28 @@ var IN_MEMORY_SEQ = 1;
 var IN_MEMORY_DB = [
     {"id": IN_MEMORY_SEQ++, "code": "Code 1", "firstName": "Nombre 1", "lastName": "Apellidos 1", "phonenumber": "", "email": "prueba1@prueba.com"},
     {"id": IN_MEMORY_SEQ++, "code": "Code 2", "firstName": "Nombre 2", "lastName": "Apellidos 2", "phonenumber": "", "email": "prueba2@prueba.com"},
-    {"id": IN_MEMORY_SEQ++, "code": "Code 3", "firstName": "Nombre 3", "lastName": "Apellidos 3", "phonenumber": "", "email": "prueba3@prueba.com"}
+    {"id": IN_MEMORY_SEQ++, "code": "Code 3", "firstName": "Nombre 3", "lastName": "Apellidos 3", "phonenumber": "", "email": "prueba3@prueba.com"},
+    {"id": IN_MEMORY_SEQ++, "code": "Code 4", "firstName": "Nombre 4", "lastName": "Apellidos 4", "phonenumber": "", "email": "prueba4@prueba.com"},
+    {"id": IN_MEMORY_SEQ++, "code": "Code 5", "firstName": "Nombre 5", "lastName": "Apellidos 5", "phonenumber": "", "email": "prueba5@prueba.com"},
+    {"id": IN_MEMORY_SEQ++, "code": "Code 6", "firstName": "Nombre 6", "lastName": "Apellidos 6", "phonenumber": "", "email": "prueba6@prueba.com"},
+    {"id": IN_MEMORY_SEQ++, "code": "Code 7", "firstName": "Nombre 7", "lastName": "Apellidos 7", "phonenumber": "", "email": "prueba7@prueba.com"},
+    {"id": IN_MEMORY_SEQ++, "code": "Code 8", "firstName": "Nombre 8", "lastName": "Apellidos 8", "phonenumber": "", "email": "prueba8@prueba.com"},
+    {"id": IN_MEMORY_SEQ++, "code": "Code 9", "firstName": "Nombre 9", "lastName": "Apellidos 9", "phonenumber": "", "email": "prueba9@prueba.com"},
+    {"id": IN_MEMORY_SEQ++, "code": "Code 10", "firstName": "Nombre 10", "lastName": "Apellidos 10", "phonenumber": "", "email": "prueba10@prueba.com"},
+    {"id": IN_MEMORY_SEQ++, "code": "Code 11", "firstName": "Nombre 11", "lastName": "Apellidos 11", "phonenumber": "", "email": "prueba11@prueba.com"},
+    {"id": IN_MEMORY_SEQ++, "code": "Code 12", "firstName": "Nombre 12", "lastName": "Apellidos 12", "phonenumber": "", "email": "prueba12@prueba.com"}
 ];
 
 angular.module('cpFacturasApp').factory('customerService', ['$resource', '$filter', function($resource, $filter) {
   return {
-    "search": function () {
-        return IN_MEMORY_DB
+    "search": function (page, elementsPerPage) {
+        var firstElement = page * elementsPerPage;
+        var lastElement = firstElement + elementsPerPage - 1;
+        var page = {
+            "data" : IN_MEMORY_DB.slice(firstElement, lastElement),
+            "total" : IN_MEMORY_DB.length
+        };
+        return page;
     },
     "save" : function (model) {
         var exist = $filter('filter')(IN_MEMORY_DB, function(item){return item.id == model.id})[0];

e2e9f7386a52c9c046e4f1705940d05fa7d49c08
FJTORRES <kisco1984@gmail.com>
2015-03-29 12:13:27 +0200

    - Refactor customer controller in two files.

--- js/app/customers/customerController.js
+++ js/app/customers/customerController.js
@@ -6,14 +6,7 @@
 
 	    var vm = this;
 	    vm.entity = {'id': -1,'type': 'PERSON', 'contactData': {}};
-	    
-	    vm.items = [];
 	    vm.isUpdate = false;
-	    vm.currentPage = 1;
-	    vm.totalItems = 0;
-	    vm.itemsPerPage = 10;
-	    vm.sortField = 'firstName';
-	    vm.sortDirection = 'ASC';
 
 	    if ($routeParams.itemId != undefined) {
 	        vm.model = customerService.resource.get({'key': $routeParams.itemId});
@@ -38,50 +31,9 @@
 	        }
 	    };
 
-	    vm.onConfirmDeleteClick = function(item){
-	        
-	        var modalInstance = $modal.open({
-	          templateUrl: 'js/app/main/confirmation.tpl.html',
-	          controller: 'confirmController',
-	          size: 'md',
-	          resolve: {
-	            title: function(){return "messages.confirmation.title";},
-	            body: function () {return "customers.messages.delete.confirmation";}
-	          }
-	        });
-
-	        modalInstance.result.then(function () {
-	          vm.onDelete(item);
-	        }, function () {
-	          // Empty
-	        });
-	    };
-
-	    vm.onDelete = function (item) {
-	        customerService.resource.delete({"id":item.id}, function () {
-	        	genericService.translate('customers.messages.delete.success', function (text) {
-            		$rootScope.$broadcast('successMessage', text);
-            	});
-	        	vm.onSearch();
-	        });
-	    };
-
-	    vm.onSearch = function () {
-	        customerService.resource.search({'page': vm.currentPage - 1, 'pageSize': vm.itemsPerPage, 'sortField': vm.sortField, 'sortDirection': vm.sortDirection}, function (result) {
-	            vm.items = result.list;
-	            vm.totalItems = result.total;
-	        });
-	        
-	    };
-
-	    vm.onPageChanged = function () {
-	        vm.onSearch();
-	    };
-
-	    vm.onSearch();
 	}
   
-angular.module('cpFacturasApp').controller('customersController', ['$rootScope', '$routeParams', '$modal', 'genericService', 'customerService', CustomersController]);
+  angular.module('cpFacturasApp').controller('customersController', ['$rootScope', '$routeParams', '$modal', 'genericService', 'customerService', CustomersController]);
 
 
 }());
